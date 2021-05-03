29123c3f12c5797fba16d5d5f28f91c40c01fc5b
z0oba <mindworkzm@gmail.com>
2021-05-03 16:20:33 +0300

    Home work 15: remove home work 14 from commit of 15

--- home-work-spring-14/src/main/java/ru/otus/homework/repo/BookRepositoryMongo.java
+++
@@ -1,8 +0,0 @@
-package ru.otus.homework.repo;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import ru.otus.homework.domain.Book;
-
-public interface BookRepositoryMongo extends MongoRepository<Book, String> {
-}
-
