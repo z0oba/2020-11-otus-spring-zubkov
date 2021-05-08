e66e44d1864eff519ab842d63867cf5cf986c7b2
mindwork <manora@yandex.ru>
2021-05-08 13:31:45 +0300

    home-work-18: rc

--- home-work-spring-18/src/main/java/ru/otus/homework/service/BookServiceImpl.java
+++ home-work-spring-18/src/main/java/ru/otus/homework/service/BookServiceImpl.java
@@ -1,5 +1,6 @@
 package ru.otus.homework.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,12 +24,14 @@ public class BookServiceImpl implements BookService {
 
     @Override
     @Transactional(readOnly = true)
+    @HystrixCommand(groupKey = "bookService", fallbackMethod = "getAllFallback", commandKey = "getAll")
     public List<Book> getAll() {
         return bookRepository.findAll();
     }
 
     @Override
     @Transactional(readOnly = true)
+    @HystrixCommand(groupKey = "bookService", fallbackMethod = "getByIdFallback", commandKey = "getById")
     public Book getById(long id) {
         return bookRepository.findById(id).orElseThrow(() -> {
             throw new BookServiceException("Can`t find book by id");
@@ -37,26 +40,30 @@ public class BookServiceImpl implements BookService {
 
     @Override
     @Transactional(readOnly = true)
+    @HystrixCommand(groupKey = "bookService", fallbackMethod = "countFallback", commandKey = "count")
     public long count() {
         return bookRepository.count();
     }
 
+
     @Override
     @Transactional
+    @HystrixCommand(groupKey = "bookService", fallbackMethod = "addFallback", commandKey = "add")
     public Book add(String name, String authorName, String genreName) {
         Author author = authorRepository.findByName(authorName);
         if (author == null)
-            author = authorRepository.save(new Author(authorName));
+            author = new Author(authorName);
 
         Genre genre = genreRepository.findByName(genreName);
         if (genre == null)
-            genre = genreRepository.save(new Genre(genreName));
+            genre = new Genre(genreName);
 
         return bookRepository.save(new Book(name, author, genre));
     }
 
     @Override
     @Transactional
+    @HystrixCommand(groupKey = "bookService", fallbackMethod = "updateByIdFallback", commandKey = "updateById")
     public long updateById(long id, String name, String authorName, String genreName) {
         if (bookRepository.findById(id).isEmpty())
             throw new BookServiceException("Can`t find book by id for update");
@@ -65,11 +72,11 @@ public class BookServiceImpl implements BookService {
 
             Author author = authorRepository.findByName(authorName);
             if (author == null)
-                author = authorRepository.save(new Author(authorName));
+                author = new Author(authorName);
 
             Genre genre = genreRepository.findByName(genreName);
             if (genre == null)
-                genre = genreRepository.save(new Genre(genreName));
+                genre = new Genre(genreName);
 
             book.setName(name);
             book.setAuthor(author);
@@ -81,7 +88,41 @@ public class BookServiceImpl implements BookService {
 
     @Override
     @Transactional
+    @HystrixCommand(groupKey = "bookService", fallbackMethod = "deleteByIdFallback", commandKey = "deleteById")
     public void deleteById(long id) {
         bookRepository.deleteById(id);
+    }
+
+
+    public List<Book> getAllFallback() {
+        return List.of(getEmptyBook());
+    }
+
+    public Book getByIdFallback(long id) {
+        return getEmptyBook();
+    }
+
+    public long countFallback() {
+        return -1;
+    }
+
+    public Book addFallback(String name, String authorName, String genreName) {
+        Book book = getEmptyBook();
+        book.setName(name);
+        book.getAuthor().setName(authorName);
+        book.getGenre().setName(genreName);
+        return book;
+    }
+
+    public long updateByIdFallback(long id, String name, String authorName, String genreName) {
+        return -1;
+    }
+
+    public void deleteByIdFallback(long id) {
+        throw new BookServiceException("Can not delete book by id " + id);
+    }
+
+    private Book getEmptyBook() {
+        return new Book(0, "N/A", new Author(0, "N/A"), new Genre(0, "N/A"));
     }
 }
--- home-work-spring-18/src/main/java/ru/otus/homework/service/UserService.java
+++
@@ -1,6 +0,0 @@
-package ru.otus.homework.service;
-
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface UserService extends UserDetailsService {
-}
